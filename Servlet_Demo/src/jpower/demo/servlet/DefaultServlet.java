dbecf3ca42bd0fbb07729396b8fe57f64d838b7c
jPower05 <jamespbaaah05@gmail.com>
2019-02-03 20:00:54 +0000

    Initial commit. Demonstrates basic functionality

---
+++ Servlet_Demo/src/jpower/demo/servlet/DefaultServlet.java
@@ -0,0 +1,45 @@
+package jpower.demo.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+//THE DEFAULT PAGE DISPLAYED IF AN INCORRECT URL IS SUPPLIED
+ 
+@WebServlet(urlPatterns = { "/" })
+public class DefaultServlet extends HttpServlet {
+ 
+   private static final long serialVersionUID = 1L;
+ 
+   public DefaultServlet() {
+   }
+ 
+   @Override
+   protected void doGet(HttpServletRequest request, HttpServletResponse response)
+           throws ServletException, IOException {
+ 
+       ServletOutputStream out = response.getOutputStream();
+ 
+       out.println("<html>");
+       out.println("<head><title>Page not found</title></head>");
+ 
+       out.println("<body>");
+       out.println("<h3>Sorry! Page not found</h3>");
+       out.println("<h1>404</h1>");
+       out.println("Message from servlet: " + this.getClass().getName());
+       out.println("</body>");
+       out.println("<html>");
+   }
+ 
+   @Override
+   protected void doPost(HttpServletRequest request, HttpServletResponse response)
+           throws ServletException, IOException {
+       this.doGet(request, response);
+   }
+}
